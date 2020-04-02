e0ae6816db5881bf9e8d7d898f2db6f2b23351ef
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-04-02 10:33:14 -0500

    add while loop and reset robot health

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,10 @@ var enemyHealth = 50;
 var enemyAttack =12;
 
 var fight = function(enemyName) {
+    // repeat and execute as long as the enemy robot is alive 
+    while(enemyHealth > 0) {
     // Alert users that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -65,8 +67,13 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to pick a valid option. Try again!")
     }
+    }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+  // call fight function with enemy robot
     fight(enemyNames[i]);
 }
